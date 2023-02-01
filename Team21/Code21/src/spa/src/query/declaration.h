9b2a27a263ffb333cf714bea55aa0c0b981d9ad7
Danh Nguyen <ducdanh24112001@gmail.com>
2023-02-01 16:25:51 +0800

    Declaration Implementation
    feat: Add Declaration class

---
+++ Team21/Code21/src/spa/src/query/declaration.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "query/design_entity.h"
+#include "query/synonym.h"
+#include <optional>
+
+namespace qps {
+
+class Declaration {
+
+	private:
+		const DesignEntity design_entity;
+		const Synonym synonym;
+
+	public:
+		DesignEntity getDesignEntity();
+		Synonym getSynonym();
+		Declaration(DesignEntity design_entity_, Synonym synonym_);
+
+		bool operator==(const Declaration& declaration2) const {
+			return design_entity == declaration2.design_entity && synonym == declaration2.synonym;
+		}
+
+		friend std::ostream& operator<<(std::ostream& os, Declaration const& declaration) {
+			os << getString(declaration.design_entity) << " " << declaration.synonym;
+			return os;
+		}
+
+
+		static std::optional<Declaration> Declaration::findDeclarationWithSynonym(std::vector<Declaration>& decl_list, Synonym& synonym) {
+			return findDeclarationWithString(decl_list, synonym.getSynonym());
+		}
+
+		static std::optional<Declaration> Declaration::findDeclarationWithString(std::vector<Declaration>& decl_list, std::string& syn_string) {
+			for (int i = 0; i < decl_list.size(); i++) {
+				if (decl_list[i].getSynonym().getSynonym() == syn_string) {
+					return std::make_optional<Declaration>(decl_list[i]);
+				}
+			}
+			return std::nullopt;
+		}
+};
+}
--- Team21/Code21/src/spa/src/query/synonym.cpp
+++ Team21/Code21/src/spa/src/query/synonym.cpp
@@ -8,6 +8,10 @@ bool Synonym::isValidSynonym(std::string syn) {
 	return std::regex_match(syn, std::regex("[a-zA-Z]([a-zA-Z]|\\d)*"));
 }
 
+std::string Synonym::getSynonym() {
+	return synonym;
+}
+
 Synonym::Synonym(std::string syn) : synonym{ syn } {
 	if (!Synonym::isValidSynonym(syn)) {
 		throw QueryException("Invalid synonym: " + syn);

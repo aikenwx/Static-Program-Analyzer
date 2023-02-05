4616dbc5d64247aeaff229fbb52564b0cbbee82e
me@zhongfu.li <me@zhongfu.li>
2023-02-05 19:10:22 +0800

    style: add newline at EOF

--- Team21/Code21/src/spa/src/exceptions/syntax_error.cpp
+++ Team21/Code21/src/spa/src/exceptions/syntax_error.cpp
@@ -4,4 +4,5 @@
 
 namespace exceptions {
 SyntaxError::SyntaxError(std::string message) { this->message = message; }
+const char* SyntaxError::what() const throw() { return "Syntax error"; }
 }  // namespace exceptions

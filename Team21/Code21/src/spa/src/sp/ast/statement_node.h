2d781b7fc6c9991e9374cd86cbb53bb41fae4c65
Spyobird <elliotlimzy@gmail.com>
2023-02-07 02:50:54 +0800

    refactor(ast): fix merge files

--- Team21/Code21/src/spa/src/sp/ast/statement_node.h
+++ Team21/Code21/src/spa/src/sp/ast/statement_node.h
@@ -3,6 +3,11 @@
 
 namespace ast {
 class StatementNode : public INode {
+public:
+  int GetStatementNumber();
+  void SetStatementNumber(int statementNumber);
+protected:
+  int statementNumber;
 
   virtual std::ostream &Write(std::ostream &out) const override = 0;
 };

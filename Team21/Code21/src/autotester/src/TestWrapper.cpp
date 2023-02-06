e026c2636cf7ee8c34adabb4ba7c72d17e87b07e
me@zhongfu.li <me@zhongfu.li>
2023-02-07 06:32:59 +0800

    feat(autotester): partial integration of tokenizer/parser/DE

--- Team21/Code21/src/autotester/src/TestWrapper.cpp
+++ Team21/Code21/src/autotester/src/TestWrapper.cpp
@@ -1,5 +1,18 @@
 #include "TestWrapper.h"
 
+#include <fstream>
+#include <sstream>
+
+#include "sp/ast/ast.h"
+#include "sp/design_extractor/directly_modifies_extractor.h"
+#include "sp/design_extractor/directly_uses_extractor.h"
+#include "sp/design_extractor/follows_extractor.h"
+#include "sp/design_extractor/parent_extractor.h"
+#include "sp/design_extractor/traverse.h"
+#include "sp/parser/simple_parser.h"
+#include "token/token.h"
+#include "tokenizer/simple_tokenizer.h"
+
 // implementation code of WrapperFactory - do NOT modify the next 5 lines
 AbstractWrapper* WrapperFactory::wrapper = 0;
 AbstractWrapper* WrapperFactory::createWrapper() {
@@ -17,13 +30,48 @@ TestWrapper::TestWrapper() {
 
 // method for parsing the SIMPLE source
 void TestWrapper::parse(std::string filename) {
-	// call your parser to do the parsing
-  // ...rest of your code...
+  // read file into string
+  std::ifstream t(filename);
+  std::stringstream buf;
+  buf << t.rdbuf();
+  std::string program = buf.str();
+
+  // tokenize the string
+  tokenizer::SimpleTokenizer* tokenizer =
+      tokenizer::SimpleTokenizer::getInstance();
+  std::vector<token::Token*> tokens = tokenizer->tokenize(program);
+
+  // parse tokens into AST
+  parser::SimpleParser* parser = new parser::SimpleParser();
+  ast::AST* ast = parser->Parse(tokens);
+
+  // process AST to find relationships
+  std::vector<rel::Relationship> followsRelationships =
+      design_extractor::Traverse(
+          ast->GetRoot(), design_extractor::FollowsExtractor::GetInstance());
+  std::vector<rel::Relationship> parentRelationships =
+      design_extractor::Traverse(
+          ast->GetRoot(), design_extractor::ParentExtractor::GetInstance());
+  std::vector<rel::Relationship> modifiesRelationships =
+      design_extractor::Traverse(
+          ast->GetRoot(),
+          design_extractor::DirectlyModifiesExtractor::GetInstance());
+  std::vector<rel::Relationship> usesRelationships = design_extractor::Traverse(
+      ast->GetRoot(), design_extractor::DirectlyUsesExtractor::GetInstance());
+
+  // init PKB
+  // TODO
+
+  // put AST entities into PKB
+  // TODO
+
+  // put relationships into PKB
+  // TODO
 }
 
 // method to evaluating a query
-void TestWrapper::evaluate(std::string query, std::list<std::string>& results){
-// call your evaluator to evaluate the query here
+void TestWrapper::evaluate(std::string query, std::list<std::string>& results) {
+  // call your evaluator to evaluate the query here
   // ...code to evaluate query...
 
   // store the answers to the query in the results list (it is initially empty)

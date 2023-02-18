3fbba5c7e83b57527cccc13bd23d56d8acb2d1c3
dhshah1 <19668322+dhshah1@users.noreply.github.com>
2023-02-19 01:34:54 +0800

    fix compile issues

--- Team21/Code21/src/spa/src/query_evaluators/pattern_evaluators/pattern_evaluator_factory.cpp
+++ Team21/Code21/src/spa/src/query_evaluators/pattern_evaluators/pattern_evaluator_factory.cpp
@@ -5,14 +5,15 @@
 #include "query/query_exceptions.h"
 
 namespace qps {
-    std::unique_ptr<PatternEvaluator> PatternEvaluatorFactory::Create(PatternClause& clause,
-        std::vector<Declaration>& decl_lst) {
-        DesignEntity d = Declaration::findDeclarationWithSynonym(decl_lst, clause.getAssign()).value().getDesignEntity();
-        switch (d) {
-        case DesignEntity::ASSIGN:return std::make_unique<AssignEvaluator>(clause, decl_lst);
-        case DesignEntity::WHILE:
-        case DesignEntity::IF:
-        default:throw QueryException(ErrorType::Syntactic, "Syntactic error. Invalid type for pattern synonym");
-        }
-    }
+std::unique_ptr<PatternEvaluator> PatternEvaluatorFactory::Create(PatternClause &clause,
+                                                                  std::vector<Declaration> &decl_lst) {
+  auto syn = clause.getAssign();
+  DesignEntity d = Declaration::findDeclarationWithSynonym(decl_lst, syn).value().getDesignEntity();
+  switch (d) {
+    case DesignEntity::ASSIGN:return std::make_unique<AssignEvaluator>(clause, decl_lst);
+    case DesignEntity::WHILE:
+    case DesignEntity::IF:
+    default:throw QueryException(ErrorType::Syntactic, "Syntactic error. Invalid type for pattern synonym");
+  }
+}
 } // qps

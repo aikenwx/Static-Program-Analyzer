dde11f58747fe41509dd4fd3b663ec6f47959307
me@zhongfu.li <me@zhongfu.li>
2023-02-18 19:00:57 +0800

    feat: add instance_of for unique ptrs

--- Team21/Code21/src/spa/src/util/instance_of.h
+++ Team21/Code21/src/spa/src/util/instance_of.h
@@ -15,4 +15,9 @@ namespace util {
   inline bool instance_of(std::shared_ptr<T> ptr) {
     return std::dynamic_pointer_cast<const Base>(ptr) != nullptr;
   }
+
+  template<typename Base, typename T>
+  inline bool instance_of(const std::unique_ptr<T>& ptr) {
+    return dynamic_cast<const Base*>(ptr.get()) != nullptr;
+  }
 }

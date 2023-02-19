e58c0e6717962e6cb6981ff06da8bc1d93f14296
me@zhongfu.li <me@zhongfu.li>
2023-02-20 04:20:44 +0800

    fix(sp): AssignExp{Extractor,Relationship} bugs

--- Team21/Code21/src/spa/src/sp/design_extractor/assign_exp_extractor.cpp
+++ Team21/Code21/src/spa/src/sp/design_extractor/assign_exp_extractor.cpp
@@ -1,22 +1,50 @@
 #include "assign_exp_extractor.h"
 
+#include <stack>
+
+#include "../ast/binary_operation_node.h"
+#include "../ast/plus_node.h"
+#include "../ast/minus_node.h"
+#include "../ast/times_node.h"
+#include "../ast/divide_node.h"
+#include "../ast/modulo_node.h"
+#include "../ast/constant_node.h"
+#include "../ast/variable_node.h"
+#include "../rel/assign_exp_relationship.h"
 #include "../rel/relationship.h"
 #include "util/instance_of.h"
 
 namespace design_extractor {
+std::string BinExpExprNodeToOperator(std::shared_ptr<ast::BinaryOperationNode> node) {
+  if (util::instance_of<ast::PlusNode>(node)) {
+    return "+";
+  } else if (util::instance_of<ast::MinusNode>(node)) {
+    return "-";
+  } else if (util::instance_of<ast::TimesNode>(node)) {
+    return "*";
+  } else if (util::instance_of<ast::DivideNode>(node)) {
+    return "/";
+  } else if (util::instance_of<ast::ModuloNode>(node)) {
+    return "%";
+  } else {
+    return "";
+  }
+}
+
 std::stack<std::string> AssignExpToPostfixExpStack(std::shared_ptr<ast::INode> node) {
   std::stack<std::string> postfixExpStack;
 
   if (util::instance_of<ast::ConstantNode>(node)) {
     std::shared_ptr<ast::ConstantNode> constantNode = std::static_pointer_cast<ast::ConstantNode>(node);
-    postfixExpStack.push(std::format('"%d"', constantNode->GetValue()));
+    // no std::format before c++20, unfortunate
+    postfixExpStack.push('"' + std::to_string(constantNode->GetValue()) + '"');
   } else if (util::instance_of<ast::VariableNode>(node)) {
     std::shared_ptr<ast::VariableNode> variableNode = std::static_pointer_cast<ast::VariableNode>(node);
-    postfixExpStack.push(std::format('"%s"', variableNode->GetName()));
-  } else if (util::instance_of<ast::BinaryExpressionNode>(node)) {
-    std::shared_ptr<ast::BinaryExpressionNode> binaryExpressionNode = std::static_pointer_cast<ast::BinaryExpressionNode>(node);
-    std::stack<std::string> leftExpStack = AssignExpToPostfixExpStack(binaryExpressionNode->GetLeft());
-    std::stack<std::string> rightExpStack = AssignExpToPostfixExpStack(binaryExpressionNode->GetRight());
+    postfixExpStack.push('"' + variableNode->GetName() + '"');
+  } else if (util::instance_of<ast::BinaryOperationNode>(node)) {
+    std::shared_ptr<ast::BinaryOperationNode> binaryOperationNode = std::static_pointer_cast<ast::BinaryOperationNode>(node);
+    std::stack<std::string> leftExpStack = AssignExpToPostfixExpStack(binaryOperationNode->GetLeft());
+    std::stack<std::string> rightExpStack = AssignExpToPostfixExpStack(binaryOperationNode->GetRight());
 
     while (!leftExpStack.empty()) {
       postfixExpStack.push(leftExpStack.top());
@@ -27,7 +55,7 @@ std::stack<std::string> AssignExpToPostfixExpStack(std::shared_ptr<ast::INode> n
       rightExpStack.pop();
     }
 
-    postfixExpStack.push(BinExpExprNodeToOperator(binaryExpressionNode));
+    postfixExpStack.push(BinExpExprNodeToOperator(binaryOperationNode));
   }
   return postfixExpStack;
 }
@@ -44,30 +72,13 @@ std::string AssignExpToPostfixExp(std::shared_ptr<ast::INode> node) {
   return postfixExp;
 }
 
-std::string BinExpExprNodeToOperator(std::shared_ptr<ast::BinaryExpressionNode> node) {
-  if (util::instance_of<ast::PlusNode>(node)) {
-    return "+";
-  } else if (util::instance_of<ast::MinusNode>(node)) {
-    return "-";
-  } else if (util::instance_of<ast::TimesNode>(node)) {
-    return "*";
-  } else if (util::instance_of<ast::DivideNode>(node)) {
-    return "/";
-  } else if (util::instance_of<ast::ModuloNode>(node)) {
-    return "%";
-  } else {
-    return "";
-  }
-}
-
-
 std::optional<std::vector<std::unique_ptr<rel::Relationship>>>
 AssignExpExtractor::HandleAssignNode(
     std::vector<std::shared_ptr<ast::INode>> parents,
     std::shared_ptr<ast::AssignNode> node) {
   std::vector<std::unique_ptr<rel::Relationship>> relationships;
 
-  std::string postfixExp = AssignExpToPostfixExp(node->GetExpression());
+  std::string postfixExp = AssignExpToPostfixExp(node->GetAssignment());
   std::unique_ptr<rel::Relationship> relationship = rel::AssignExpRelationship::CreateRelationship(node, postfixExp);
   relationships.push_back(std::move(relationship));
 
--- Team21/Code21/src/spa/src/sp/rel/assign_exp_relationship.h
+++ Team21/Code21/src/spa/src/sp/rel/assign_exp_relationship.h
@@ -8,15 +8,15 @@
 namespace rel {
 class AssignExpRelationship : public Relationship {
  public:
-  static std::unique_ptr<AssignExpRelationship> CreateRelationship(std::shared_ptr<ast::AssignNode> assignNode);
+  static std::unique_ptr<AssignExpRelationship> CreateRelationship(std::shared_ptr<ast::AssignNode> assignNode, std::string postfixExp);
   std::shared_ptr<ast::AssignNode> assignNode();
   std::string varName();
   int statementNumber();
-  std::string postfixExp_();
+  std::string postfixExp();
   RelationshipType relationshipType() override { return RelationshipType::PROC; };
 
  private:
-  AssignExpRelationship(std::shared_ptr<ast::AssignNode> assignNode);
+  AssignExpRelationship(std::shared_ptr<ast::AssignNode> assignNode, std::string postfixExp);
   std::shared_ptr<ast::AssignNode> assignNode_;
   std::string postfixExp_;
 };

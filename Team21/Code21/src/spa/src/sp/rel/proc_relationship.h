63b6b686793d927f9df52bc2f23f52658b777988
me@zhongfu.li <me@zhongfu.li>
2023-02-20 02:08:47 +0800

    feat(sp): store ProcedureNode ref in ProcRelationship

--- Team21/Code21/src/spa/src/sp/rel/proc_relationship.h
+++ Team21/Code21/src/spa/src/sp/rel/proc_relationship.h
@@ -7,12 +7,13 @@
 namespace rel {
 class ProcRelationship : public Relationship {
  public:
-  static std::unique_ptr<ProcRelationship> CreateRelationship(std::string procedureName);
+  static std::unique_ptr<ProcRelationship> CreateRelationship(std::shared_ptr<ast::ProcedureNode> procedureNode);
+  std::shared_ptr<ast::ProcedureNode> procedureNode();
   std::string procedureName();
   RelationshipType relationshipType() override { return RelationshipType::PROC; };
 
  private:
-  ProcRelationship(std::string procedureName);
-  std::string procedureName_;
+  ProcRelationship(std::shared_ptr<ast::ProcedureNode> procedureNode);
+  std::shared_ptr<ast::ProcedureNode> procedureNode_;
 };
 } // namespace rel

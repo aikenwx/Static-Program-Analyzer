9fb636bc793a125eb0777b2841768e1130b0b1bf
me@zhongfu.li <me@zhongfu.li>
2023-02-19 15:48:09 +0800

    feat(sp): add new StmtRelationship types for Assign/Call/If/While

--- Team21/Code21/src/spa/src/sp/rel/stmt_relationship.cpp
+++ Team21/Code21/src/spa/src/sp/rel/stmt_relationship.cpp
@@ -1,6 +1,38 @@
 #include "stmt_relationship.h"
 
 namespace rel {
+std::unique_ptr<AssignStmtRelationship> AssignStmtRelationship::CreateRelationship(std::shared_ptr<ast::AssignNode> statementNode) {
+  return std::unique_ptr<AssignStmtRelationship>(new AssignStmtRelationship(statementNode));
+};
+
+AssignStmtRelationship::AssignStmtRelationship(std::shared_ptr<ast::AssignNode> statementNode) {
+  statementNode_ = statementNode;
+};
+
+std::unique_ptr<CallStmtRelationship> CallStmtRelationship::CreateRelationship(std::shared_ptr<ast::CallNode> statementNode) {
+  return std::unique_ptr<CallStmtRelationship>(new CallStmtRelationship(statementNode));
+};
+
+CallStmtRelationship::CallStmtRelationship(std::shared_ptr<ast::CallNode> statementNode) {
+  statementNode_ = statementNode;
+};
+
+std::unique_ptr<IfStmtRelationship> IfStmtRelationship::CreateRelationship(std::shared_ptr<ast::IfNode> statementNode) {
+  return std::unique_ptr<IfStmtRelationship>(new IfStmtRelationship(statementNode));
+};
+
+IfStmtRelationship::IfStmtRelationship(std::shared_ptr<ast::IfNode> statementNode) {
+  statementNode_ = statementNode;
+};
+
+std::unique_ptr<WhileStmtRelationship> WhileStmtRelationship::CreateRelationship(std::shared_ptr<ast::WhileNode> statementNode) {
+  return std::unique_ptr<WhileStmtRelationship>(new WhileStmtRelationship(statementNode));
+};
+
+WhileStmtRelationship::WhileStmtRelationship(std::shared_ptr<ast::WhileNode> statementNode) {
+  statementNode_ = statementNode;
+};
+
 std::unique_ptr<PrintStmtRelationship> PrintStmtRelationship::CreateRelationship(std::shared_ptr<ast::PrintNode> statementNode) {
   return std::unique_ptr<PrintStmtRelationship>(new PrintStmtRelationship(statementNode));
 };
--- Team21/Code21/src/spa/src/sp/rel/stmt_relationship.h
+++ Team21/Code21/src/spa/src/sp/rel/stmt_relationship.h
@@ -3,10 +3,50 @@
 #include <string>
 
 #include "relationship.h"
+#include "../ast/assign_node.h"
+#include "../ast/call_node.h"
+#include "../ast/if_node.h"
+#include "../ast/while_node.h"
 #include "../ast/print_node.h"
 #include "../ast/read_node.h"
 
 namespace rel {
+class AssignStmtRelationship : public StmtRelationship {
+ public:
+  static std::unique_ptr<AssignStmtRelationship> CreateRelationship(std::shared_ptr<ast::AssignNode> statementNode);
+
+ private:
+  AssignStmtRelationship(std::shared_ptr<ast::AssignNode> statementNode);
+  std::shared_ptr<ast::AssignNode> statementNode_;
+};
+
+class CallStmtRelationship : public StmtRelationship {
+ public:
+  static std::unique_ptr<CallStmtRelationship> CreateRelationship(std::shared_ptr<ast::CallNode> statementNode);
+
+ private:
+  CallStmtRelationship(std::shared_ptr<ast::CallNode> statementNode);
+  std::shared_ptr<ast::CallNode> statementNode_;
+};
+
+class IfStmtRelationship : public StmtRelationship {
+ public:
+  static std::unique_ptr<IfStmtRelationship> CreateRelationship(std::shared_ptr<ast::IfNode> statementNode);
+
+ private:
+  IfStmtRelationship(std::shared_ptr<ast::IfNode> statementNode);
+  std::shared_ptr<ast::IfNode> statementNode_;
+};
+
+class WhileStmtRelationship : public StmtRelationship {
+ public:
+  static std::unique_ptr<WhileStmtRelationship> CreateRelationship(std::shared_ptr<ast::WhileNode> statementNode);
+
+ private:
+  WhileStmtRelationship(std::shared_ptr<ast::WhileNode> statementNode);
+  std::shared_ptr<ast::WhileNode> statementNode_;
+};
+
 class PrintStmtRelationship : public StmtRelationship {
  public:
   static std::unique_ptr<PrintStmtRelationship> CreateRelationship(

b91b30bb0f69eb439f32e9d765d21b4f8b58b52a
me@zhongfu.li <me@zhongfu.li>
2023-03-05 14:41:43 +0800

    refactor: IntervalTree inclusion
    (templated types get super iffy if new typevars are used from a different project)

--- Team21/Code21/src/spa/src/util/interval_tree.h
+++ Team21/Code21/src/spa/src/util/interval_tree.h
@@ -1,7 +1,8 @@
 #pragma once
 
-#include <optional>
+#include <algorithm>
 #include <map>
+#include <optional>
 #include <utility>
 
 namespace util {
@@ -9,13 +10,34 @@ template <typename Key, typename Val>
 class IntervalTree {
   /**
    * A interval tree with intervals of type Key and values of type Val.
-   * The intervals are inclusive, and should not overlap.
+   * The intervals are inclusive, and (probably) should not overlap.
    */
  public:
-  IntervalTree();
-  void Insert(std::pair<Key, Key> interval, Val value);
-  void Delete(std::pair<Key, Key> interval);
-  std::optional<Val> Search(Key value);
+  IntervalTree(){};
+  bool Insert(std::pair<Key, Key> interval, Val value) {
+    if (interval.first > interval.second) {
+      return false;
+    }
+    if (Search(interval.first) != std::nullopt ||
+        Search(interval.second) != std::nullopt) {
+      return false;
+    }
+    intervals_.insert({interval, value});
+    return true;
+  }
+  bool Delete(std::pair<Key, Key> interval) {
+    return intervals_.erase(interval) > 0;
+  };
+  std::optional<Val> Search(Key key) {
+    auto it = intervals_.lower_bound({value, value});
+    if (it == intervals_.end()) {
+      return std::nullopt;
+    }
+    if (it->first.first <= value && it->first.second >= value) {
+      return it->second;
+    }
+    return std::nullopt;
+  };
 
  private:
   std::map<std::pair<Key, Key>, Val> intervals_;

f9f7c258ba2e3c82a9c7db8e9d823c21e2c3114b
me@zhongfu.li <me@zhongfu.li>
2023-03-05 15:24:36 +0800

    fix: extra checks for overlapping intervals in IntervalTree Insert()

--- Team21/Code21/src/spa/src/util/interval_tree.h
+++ Team21/Code21/src/spa/src/util/interval_tree.h
@@ -18,8 +18,7 @@ class IntervalTree {
     if (interval.first > interval.second) {
       return false;
     }
-    if (Search(interval.first) != std::nullopt ||
-        Search(interval.second) != std::nullopt) {
+    if (HasOverlap(interval)) {
       return false;
     }
     intervals_.insert({interval, value});
@@ -29,6 +28,19 @@ class IntervalTree {
     return intervals_.erase(interval) > 0;
   };
   std::optional<Val> Search(Key key) {
+    auto closest = SearchClosest(key);
+    if (closest == std::nullopt) {
+      return std::nullopt;
+    }
+    if (closest->first.first <= key && closest->first.second >= key) {
+      return closest->second;
+    }
+    return std::nullopt;
+  };
+
+ private:
+  std::map<std::pair<Key, Key>, Val> intervals_;
+  std::optional<std::pair<std::pair<Key, Key>, Val>> SearchClosest(Key key) {
     auto it = std::lower_bound(intervals_.begin(), intervals_.end(), key,
                                [](const auto& kvp1, const auto& key) {
                                  return kvp1.first.second < key;
@@ -36,13 +48,43 @@ class IntervalTree {
     if (it == intervals_.end()) {
       return std::nullopt;
     }
-    if (it->first.first <= key && it->first.second >= key) {
-      return it->second;
+    return *it;
+  };
+  bool HasOverlap(std::pair<Key, Key> interval1) {
+    std::optional<std::pair<std::pair<Key, Key>, Val>> closest = SearchClosest(interval1.first);
+    if (closest == std::nullopt) {
+      return false;
     }
-    return std::nullopt;
-  }
-
- private:
-  std::map<std::pair<Key, Key>, Val> intervals_;
+    if (Overlaps(interval1, closest->first)) {
+      return true;
+    }
+    closest = SearchClosest(interval1.second);
+    if (closest == std::nullopt) {
+      return false;
+    }
+    if (Overlaps(interval1, closest->first)) {
+      return true;
+    }
+    return false;
+  };
+  bool Overlaps(std::pair<Key, Key> interval1, std::pair<Key, Key> interval2) {
+    // interval1 starts somewhere within interval2
+    if (interval1.first >= interval2.first && interval1.first <= interval2.second) {
+      return true;
+    }
+    // interval1 ends somewhere within interval2
+    if (interval1.second >= interval2.first && interval1.second <= interval2.second) {
+      return true;
+    }
+    // interval2 starts somewhere within interval1
+    if (interval2.first >= interval1.first && interval2.first <= interval1.second) {
+      return true;
+    }
+    // interval2 ends somewhere within interval1
+    if (interval2.second >= interval1.first && interval2.second <= interval1.second) {
+      return true;
+    }
+    return false;
+  };
 };
 }  // namespace util

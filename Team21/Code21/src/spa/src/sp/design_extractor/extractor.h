b329d0fe8cb0a0a5ed3bc6dcb06705426c1e1e6e
me@zhongfu.li <me@zhongfu.li>
2023-03-05 18:55:19 +0800

    refactor(sp): add default impl for Extractor.Handle*()

--- Team21/Code21/src/spa/src/sp/design_extractor/extractor.h
+++ Team21/Code21/src/spa/src/sp/design_extractor/extractor.h
@@ -24,26 +24,26 @@ class Extractor {
  public:
   virtual ~Extractor() = default;
   virtual void HandleAssignNode(std::shared_ptr<ast::AssignNode> node,
-                                int depth) = 0;
-  virtual void HandleCallNode(std::shared_ptr<ast::CallNode> node, int depth) = 0;
-  virtual void HandleIfNode(std::shared_ptr<ast::IfNode> node, int depth) = 0;
+                                int depth){};
+  virtual void HandleCallNode(std::shared_ptr<ast::CallNode> node, int depth){};
+  virtual void HandleIfNode(std::shared_ptr<ast::IfNode> node, int depth){};
   virtual void HandleWhileNode(std::shared_ptr<ast::WhileNode> node,
-                               int depth) = 0;
+                               int depth){};
   virtual void HandleConstantNode(std::shared_ptr<ast::ConstantNode> node,
-                                  int depth) = 0;
+                                  int depth){};
   virtual void HandlePrintNode(std::shared_ptr<ast::PrintNode> node,
-                               int depth) = 0;
+                               int depth){};
   virtual void HandleProcedureNode(std::shared_ptr<ast::ProcedureNode> node,
-                                   int depth) = 0;
+                                   int depth){};
   virtual void HandleProgramNode(std::shared_ptr<ast::ProgramNode> node,
-                                 int depth) = 0;
-  virtual void HandleReadNode(std::shared_ptr<ast::ReadNode> node, int depth) = 0;
+                                 int depth){};
+  virtual void HandleReadNode(std::shared_ptr<ast::ReadNode> node, int depth){};
   virtual void HandleStatementListNode(
-      std::shared_ptr<ast::StatementListNode> node, int depth) = 0;
+      std::shared_ptr<ast::StatementListNode> node, int depth){};
   virtual void HandleStatementNode(std::shared_ptr<ast::StatementNode> node,
-                                   int depth) = 0;
+                                   int depth){};
   virtual void HandleVariableNode(std::shared_ptr<ast::VariableNode> node,
-                                  int depth) = 0;
-  virtual void HandleOtherNode(std::shared_ptr<ast::INode> node, int depth) = 0;
+                                  int depth){};
+  virtual void HandleOtherNode(std::shared_ptr<ast::INode> node, int depth){};
 };
 }  // namespace design_extractor

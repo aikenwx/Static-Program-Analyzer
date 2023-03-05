692d1b3e5e2f0ec3db91e2306f00336dc7103773
me@zhongfu.li <me@zhongfu.li>
2023-03-05 21:07:16 +0800

    test(sp): additional tests for syntax errors

--- Team21/Code21/src/unit_testing/src/sp/sp/syntax_rules.cpp
+++ Team21/Code21/src/unit_testing/src/sp/sp/syntax_rules.cpp
@@ -3,8 +3,8 @@
 #include "exceptions/syntax_error.h"
 #include "sp/sp.h"
 
-TEST_CASE(
-    "SP should terminate there are unexpected characters in the program") {
+TEST_CASE("SP should terminate there are unexpected characters in the program",
+          "[sp]") {
   PKB pkb = PKB();
   sp::SP sp = sp::SP();
 
@@ -21,4 +21,21 @@ TEST_CASE(
 
   REQUIRE_THROWS_MATCHES(sp.process(program, &pkb), exceptions::SyntaxError,
                          Catch::Message("Syntax error: Unexpected symbol"));
+};
+
+TEST_CASE("SP should terminate if invalid programs are given", "[sp]") {
+  PKB pkb = PKB();
+  sp::SP sp = sp::SP();
+
+  std::string program = R"(procedure main {})";
+  REQUIRE_THROWS_MATCHES(sp.process(program, &pkb), exceptions::SyntaxError,
+                         Catch::Message("Syntax error: Invalid program"));
+
+  program = R"(1 + 3 == 4)";
+  REQUIRE_THROWS_MATCHES(sp.process(program, &pkb), exceptions::SyntaxError,
+                         Catch::Message("Syntax error: Invalid program"));
+
+  program = "";
+  REQUIRE_THROWS_MATCHES(sp.process(program, &pkb), exceptions::SyntaxError,
+                         Catch::Message("Syntax error: Empty program"));
 }

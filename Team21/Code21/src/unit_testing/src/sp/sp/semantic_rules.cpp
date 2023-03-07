49be594f4b56354688b6091083d099fa9a7d5aa1
me@zhongfu.li <me@zhongfu.li>
2023-03-07 10:43:44 +0800

    test(sp): add simple test for CallValidator

--- Team21/Code21/src/unit_testing/src/sp/sp/semantic_rules.cpp
+++ Team21/Code21/src/unit_testing/src/sp/sp/semantic_rules.cpp
@@ -16,9 +16,24 @@ TEST_CASE("SP should terminate if there are non-unique procedure names") {
     print x;
   })";
 
-  PKB pkb = PKB();
+  auto pkb = PKB();
 
-  sp::SP sp = sp::SP();
+  auto sp = sp::SP();
 
   REQUIRE_THROWS_MATCHES(sp.process(program, &pkb), exceptions::SemanticError, Catch::Message("Semantic error: Duplicate procedure name"));
+}
+
+TEST_CASE("SP should terminate if a nonexistent procedure is called") {
+  std::string program = R"(procedure main {
+    x = 1;
+    read y;
+    print x;
+    call nonexistent;
+  })";
+
+  auto pkb = PKB();
+
+  auto sp = sp::SP();
+
+  REQUIRE_THROWS_MATCHES(sp.process(program, &pkb), exceptions::SemanticError, Catch::Message("Semantic error: Call node cannot call procedure that does not exist"));
 }

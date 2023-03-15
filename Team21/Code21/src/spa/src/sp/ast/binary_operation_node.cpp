a24955a2408c6d19940d8d3dbf0d4d970ee14582
zhongfu <me@zhongfu.li>
2023-03-15 14:32:22 +0800

    refactor(sp/ast): hide protected data member left/right in binop nodes

--- Team21/Code21/src/spa/src/sp/ast/binary_operation_node.cpp
+++ Team21/Code21/src/spa/src/sp/ast/binary_operation_node.cpp
@@ -5,11 +5,16 @@
 namespace ast {
 BinaryOperationNode::BinaryOperationNode(std::shared_ptr<INode> left,
                                          std::shared_ptr<INode> right)
-    : left(std::move(left))), right(std::move(std::move(right)) {}
+    : left_(std::move(left)), right_(std::move(right)) {}
 
-auto BinaryOperationNode::GetLeft() -> std::shared_ptr<INode> { return left; }
+auto BinaryOperationNode::GetLeft() const -> std::shared_ptr<INode> { return left_; }
 
-auto BinaryOperationNode::GetRight() -> std::shared_ptr<INode> { return right; }
+auto BinaryOperationNode::GetRight() const -> std::shared_ptr<INode> { return right_; }
+
+auto BinaryOperationNode::ToString() const -> std::string {
+  return "{\nleft:" + left_->ToString() + "right:" + right_->ToString() +
+         "}\n";
+}
 
 void BinaryOperationNode::AcceptVisitor(
     const std::shared_ptr<INode>& currentNode,
@@ -19,7 +24,7 @@ void BinaryOperationNode::AcceptVisitor(
   // e.g. node parents
   extractor->HandleOtherNode(currentNode, depth);
 
-  left->AcceptVisitor(left, extractor, depth + 1);
-  right->AcceptVisitor(right, extractor, depth + 1);
+  left_->AcceptVisitor(left_, extractor, depth + 1);
+  right_->AcceptVisitor(right_, extractor, depth + 1);
 }
 }  // namespace ast
--- Team21/Code21/src/spa/src/sp/ast/binary_operation_node.h
+++ Team21/Code21/src/spa/src/sp/ast/binary_operation_node.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <memory>
+#include <utility>
 
 #include "i_node.h"
 
@@ -9,15 +10,19 @@ class BinaryOperationNode : public INode {
 public:
   BinaryOperationNode(std::shared_ptr<INode> left, std::shared_ptr<INode> right);
 
-  auto GetLeft() -> std::shared_ptr<INode>;
-  auto GetRight() -> std::shared_ptr<INode>;
-  [[nodiscard]] auto ToString() const -> std::string override = 0;
+  [[nodiscard]] auto GetLeft() const -> std::shared_ptr<INode>;
+  [[nodiscard]] auto GetRight() const -> std::shared_ptr<INode>;
+  [[nodiscard]] auto ToString() const -> std::string override;
   void AcceptVisitor(const std::shared_ptr<INode>& currentNode,
                      const std::shared_ptr<design_extractor::Extractor>& extractor,
                      int depth) override;
 
  protected:
-  std::shared_ptr<INode> left;
-  std::shared_ptr<INode> right;
+  void SetLeft(std::shared_ptr<INode> left) { this->left_ = std::move(left); }
+  void SetRight(std::shared_ptr<INode> right) { this->right_ = std::move(right); }
+
+ private:
+  std::shared_ptr<INode> left_;
+  std::shared_ptr<INode> right_;
 };
 }  // namespace ast

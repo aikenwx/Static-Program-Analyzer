4bdf38e2fdccda05deaf517125ba47a89ebc7185
dhshah1 <19668322+dhshah1@users.noreply.github.com>
2023-03-18 22:58:47 +0800

    feat(qps): next/next*

--- Team21/Code21/src/integration_testing/src/TestNextClauses.cpp
+++ Team21/Code21/src/integration_testing/src/TestNextClauses.cpp
@@ -24,6 +24,7 @@ namespace qps {
 //}
 
 
+
 TEST_CASE("Next Clauses") {
   qps_test::PopulatePKBHelper::Data data;
   data[qps::DesignEntity::ASSIGN] = {"1", "2", "4", "6", "8", "9", "10", "11", "12"};
@@ -44,123 +45,111 @@ TEST_CASE("Next Clauses") {
   three->AddChild(four);
   three->AddChild(seven);
   four->AddChild(three);
-//  seven->AddChild(eight);
-//  seven->AddChild(nine);
-//  eight->AddChild(ten);
-//  nine->AddChild(ten);
+  seven->AddChild(eight);
+  seven->AddChild(nine);
+  eight->AddChild(ten);
+  nine->AddChild(ten);
 
   three->AddParent(one);
   four->AddParent(three);
   seven->AddParent(three);
   three->AddParent(four);
-//  eight->AddParent(seven);
-//  nine->AddParent(seven);
-//  ten->AddParent(eight);
-//  ten->AddParent(nine);
+  eight->AddParent(seven);
+  nine->AddParent(seven);
+  ten->AddParent(eight);
+  ten->AddParent(nine);
 
   cfg->InsertBlock(one);
   cfg->InsertBlock(three);
   cfg->InsertBlock(four);
   cfg->InsertBlock(seven);
-//  cfg->InsertBlock(eight);
-//  cfg->InsertBlock(nine);
-//  cfg->InsertBlock(ten);
+  cfg->InsertBlock(eight);
+  cfg->InsertBlock(nine);
+  cfg->InsertBlock(ten);
 
   qps_test::PopulatePKBHelper pkb_helper;
   pkb_helper.PopulateEntities(data);
   pkb_helper.StoreCFG(cfg);
   QueryFacade *pkb_querier = pkb_helper.GetQuerier();
 
-//  SECTION("Next: (LIT, LIT)") {
-//    std::unordered_set<std::string> TRUE{"TRUE"};
-//    std::unordered_set<std::string> FALSE{"FALSE"};
-//    REQUIRE(qps_test::RunQuery("Select BOOLEAN such that Next(3,4)", *pkb_querier) == TRUE);
-//    REQUIRE(qps_test::RunQuery("Select BOOLEAN such that Next(3,7)", *pkb_querier) == TRUE);
-//    REQUIRE(qps_test::RunQuery("Select BOOLEAN such that Next(5,6)", *pkb_querier) == TRUE);
-//    REQUIRE(qps_test::RunQuery("Select BOOLEAN such that Next(6,3)", *pkb_querier) == TRUE);
-//    REQUIRE(qps_test::RunQuery("Select BOOLEAN such that Next(8,10)", *pkb_querier) == TRUE);
-//    REQUIRE(qps_test::RunQuery("Select BOOLEAN such that Next(9,10)", *pkb_querier) == TRUE);
-//
-//    REQUIRE(qps_test::RunQuery("Select BOOLEAN such that Next(4, 3)", *pkb_querier) == FALSE);
-//    REQUIRE(qps_test::RunQuery("Select BOOLEAN such that Next(12, 13)", *pkb_querier) == FALSE);
-//    REQUIRE(qps_test::RunQuery("Select BOOLEAN such that Next(1, 3)", *pkb_querier) == FALSE);
-//  }
+  SECTION("Next: (LIT, LIT)") {
+    std::unordered_set<std::string> TRUE{"TRUE"};
+    std::unordered_set<std::string> FALSE{"FALSE"};
+    REQUIRE(qps_test::RunQuery("Select BOOLEAN such that Next(3,4)", *pkb_querier) == TRUE);
+    REQUIRE(qps_test::RunQuery("Select BOOLEAN such that Next(3,7)", *pkb_querier) == TRUE);
+    REQUIRE(qps_test::RunQuery("Select BOOLEAN such that Next(5,6)", *pkb_querier) == TRUE);
+    REQUIRE(qps_test::RunQuery("Select BOOLEAN such that Next(6,3)", *pkb_querier) == TRUE);
+    REQUIRE(qps_test::RunQuery("Select BOOLEAN such that Next(8,10)", *pkb_querier) == TRUE);
+    REQUIRE(qps_test::RunQuery("Select BOOLEAN such that Next(9,10)", *pkb_querier) == TRUE);
 
-//  SECTION("Next: (LIT, SYN)") {
-//    std::unordered_set<std::string> expected{"4", "7"};
-//    REQUIRE(qps_test::RunQuery("stmt s; Select s such that Next(3,s)", *pkb_querier) == expected);
-//
-//    expected = {"7"};
-//    REQUIRE(qps_test::RunQuery("if f; Select f such that Next(3,f)", *pkb_querier) == expected);
-//
-//    expected = {"8", "9"};
-//    REQUIRE(qps_test::RunQuery("stmt s; Select s such that Next(7,s)", *pkb_querier) == expected);
-//  }
+    REQUIRE(qps_test::RunQuery("Select BOOLEAN such that Next(4, 3)", *pkb_querier) == FALSE);
+    REQUIRE(qps_test::RunQuery("Select BOOLEAN such that Next(12, 13)", *pkb_querier) == FALSE);
+    REQUIRE(qps_test::RunQuery("Select BOOLEAN such that Next(1, 3)", *pkb_querier) == FALSE);
+  }
 
-//  SECTION("Next: (SYN, LIT)") {
-//    std::unordered_set<std::string> expected{"7"};
-//    REQUIRE(qps_test::RunQuery("if f; Select f such that Next(f,8)", *pkb_querier) == expected);
-//  }
+  SECTION("Next: (LIT, SYN)") {
+    std::unordered_set<std::string> expected{"4", "7"};
+    REQUIRE(qps_test::RunQuery("stmt s; Select s such that Next(3,s)", *pkb_querier) == expected);
 
-//  SECTION("Next: (SYN, SYN)") {
-//    std::unordered_set<std::string> expected{"3 4", "3 7"};
-//    REQUIRE(qps_test::RunQuery("stmt s; while w; Select <w,s> such that Next(w,s)", *pkb_querier) == expected);
-//    expected = {"1 2", "2 3", "3 4", "3 7", "4 5", "5 6", "6 3", "7 8", "7 9", "8 10", "9 10",
-//                "10 11", "11 12"};
-//    REQUIRE(qps_test::RunQuery("stmt s,s1; Select <s,s1> such that Next(s,s1)", *pkb_querier) == expected);
-//  }
+    expected = {"7"};
+    REQUIRE(qps_test::RunQuery("if f; Select f such that Next(3,f)", *pkb_querier) == expected);
 
-//  SECTION("NEXT*: (LIT LIT)") {
-//    std::unordered_set<std::string> TRUE{"TRUE"};
-//    std::unordered_set<std::string> FALSE{"FALSE"};
-//
-//    REQUIRE(qps_test::RunQuery("Select BOOLEAN such that Next*(3,4)", *pkb_querier) == TRUE);
-//    REQUIRE(qps_test::RunQuery("Select BOOLEAN such that Next*(3,10)", *pkb_querier) == TRUE);
-//    REQUIRE(qps_test::RunQuery("Select BOOLEAN such that Next*(4,3)", *pkb_querier) == TRUE);
-//    REQUIRE(qps_test::RunQuery("Select BOOLEAN such that Next*(3,3)", *pkb_querier) == TRUE);
-//    REQUIRE(qps_test::RunQuery("Select BOOLEAN such that Next*(5,4)", *pkb_querier) == TRUE);
-//
-//    REQUIRE(qps_test::RunQuery("Select BOOLEAN such that Next*(8,8)", *pkb_querier) == FALSE);
-//    REQUIRE(qps_test::RunQuery("Select BOOLEAN such that Next*(8,7)", *pkb_querier) == FALSE);
-//  }
-//
-//  SECTION("NEXT*: (SYN LIT)") {
-//    std::unordered_set<std::string> expected{"5"};
-//    REQUIRE(qps_test::RunQuery("call c; Select c such that Next*(c, 8)", *pkb_querier) == expected);
-//  }
-//
-//  SECTION("NEXT*: (LIT SYN)") {
-//    std::unordered_set<std::string> expected{"4", "6", "8", "9", "10", "11", "12"};
-//    REQUIRE(qps_test::RunQuery("assign a; Select a such that Next*(4, a)", *pkb_querier) == expected);
-//  }
+    expected = {"8", "9"};
+    REQUIRE(qps_test::RunQuery("stmt s; Select s such that Next(7,s)", *pkb_querier) == expected);
+  }
+
+  SECTION("Next: (SYN, LIT)") {
+    std::unordered_set<std::string> expected{"7"};
+    REQUIRE(qps_test::RunQuery("if f; Select f such that Next(f,8)", *pkb_querier) == expected);
+  }
+
+  SECTION("Next: (SYN, SYN)") {
+    std::unordered_set<std::string> expected{"3 4", "3 7"};
+    REQUIRE(qps_test::RunQuery("stmt s; while w; Select <w,s> such that Next(w,s)", *pkb_querier) == expected);
+    expected = {"1 2", "2 3", "3 4", "3 7", "4 5", "5 6", "6 3", "7 8", "7 9", "8 10", "9 10",
+                "10 11", "11 12"};
+    REQUIRE(qps_test::RunQuery("stmt s,s1; Select <s,s1> such that Next(s,s1)", *pkb_querier) == expected);
+  }
+
+  SECTION("NEXT*: (LIT LIT)") {
+    std::unordered_set<std::string> TRUE{"TRUE"};
+    std::unordered_set<std::string> FALSE{"FALSE"};
+
+    REQUIRE(qps_test::RunQuery("Select BOOLEAN such that Next*(3,4)", *pkb_querier) == TRUE);
+    REQUIRE(qps_test::RunQuery("Select BOOLEAN such that Next*(3,10)", *pkb_querier) == TRUE);
+    REQUIRE(qps_test::RunQuery("Select BOOLEAN such that Next*(4,3)", *pkb_querier) == TRUE);
+    REQUIRE(qps_test::RunQuery("Select BOOLEAN such that Next*(3,3)", *pkb_querier) == TRUE);
+    REQUIRE(qps_test::RunQuery("Select BOOLEAN such that Next*(5,4)", *pkb_querier) == TRUE);
+
+    REQUIRE(qps_test::RunQuery("Select BOOLEAN such that Next*(8,8)", *pkb_querier) == FALSE);
+    REQUIRE(qps_test::RunQuery("Select BOOLEAN such that Next*(8,7)", *pkb_querier) == FALSE);
+  }
+
+  SECTION("NEXT*: (SYN LIT)") {
+    std::unordered_set<std::string> expected{"5"};
+    REQUIRE(qps_test::RunQuery("call c; Select c such that Next*(c, 8)", *pkb_querier) == expected);
+  }
+
+  SECTION("NEXT*: (LIT SYN)") {
+    std::unordered_set<std::string> expected{"4", "6", "8", "9", "10", "11", "12"};
+    REQUIRE(qps_test::RunQuery("assign a; Select a such that Next*(4, a)", *pkb_querier) == expected);
+  }
 
   SECTION("NEXT*: (SYN SYN)") {
-//    std::unordered_set<std::string>
-//        expected{"1 2", "1 3", "1 4", "1 5", "1 6", "1 7", "1 8", "1 9", "1 10", "1 11", "1 12",
-//                 "2 3", "2 4", "2 5", "2 6", "2 7", "2 8", "2 9", "2 10", "2 11", "2 12",
-//                 "3 4", "3 5", "3 6", "3 7", "3 8", "3 9", "3 10", "3 11", "3 12",
-//                 "4 5", "4 6", "4 7", "4 8", "4 9", "4 10", "4 11", "4 12", "4 3", "4, 4",
-//                 "5 6", "5 7", "5 8", "5 9", "5 10", "5 11", "5 12", "5 4", "5, 3", "5, 5",
-//                 "6 6", "6 7", "6 8", "6 9", "6 10", "6 11", "6 12", "6 3", "6 4", "6 5",
-//                 "7 8", "7 9", "7 10", "7 11", "7 12",
-//                 "8 10", "8 11", "8 12",
-//                 "9 10", "9 11", "9 12",
-//                 "10 11", "10 12",
-//                 "11 12"};
     std::unordered_set<std::string>
-        expected{"1 2", "1 3", "1 4", "1 5", "1 6", "1 7",
-                 "2 3", "2 4", "2 5", "2 6", "2 7",
-                 "3 4", "3 5", "3 6", "3 7",
-                 "4 5", "4 6", "4 7", "4, 3", "4, 4",
-                 "5 6", "5 7", "5, 3", "5, 4", "5, 5",
-                 "6 6", "6 7", "6 3", "6 4", "6 5"};
-    auto ans = qps_test::RunQuery("stmt s,s1; Select <s, s1> such that Next*(s, s1)", *pkb_querier);
-    std::vector<std::string> a(ans.begin(), ans.end());
-    std::vector<std::string> b(expected.begin(), expected.end());
-    std::sort(a.begin(), a.end());
-    std::sort(b.begin(), b.end());
-    REQUIRE(a == b);
-//    REQUIRE(qps_test::RunQuery("stmt s,s1; Select <s, s1> such that Next*(s, s1)", *pkb_querier) == expected);
+        expected{"1 2", "1 3", "1 4", "1 5", "1 6", "1 7", "1 8", "1 9", "1 10", "1 11", "1 12",
+                 "2 3", "2 4", "2 5", "2 6", "2 7", "2 8", "2 9", "2 10", "2 11", "2 12",
+                 "3 3", "3 4", "3 5", "3 6", "3 7", "3 8", "3 9", "3 10", "3 11", "3 12",
+                 "4 5", "4 6", "4 7", "4 8", "4 9", "4 10", "4 11", "4 12", "4 3", "4 4",
+                 "5 6", "5 7", "5 8", "5 9", "5 10", "5 11", "5 12", "5 4", "5 3", "5 5",
+                 "6 6", "6 7", "6 8", "6 9", "6 10", "6 11", "6 12", "6 3", "6 4", "6 5",
+                 "7 8", "7 9", "7 10", "7 11", "7 12",
+                 "8 10", "8 11", "8 12",
+                 "9 10", "9 11", "9 12",
+                 "10 11", "10 12",
+                 "11 12"};
+
+    REQUIRE(qps_test::RunQuery("stmt s,s1; Select <s, s1> such that Next*(s, s1)", *pkb_querier) == expected);
   }
 }
-} // qps
+}  // namespace qps
--- Team21/Code21/src/spa/src/query_evaluators/such_that_evaluators/cfg_evaluators/cfg_evaluator.h
+++ Team21/Code21/src/spa/src/query_evaluators/such_that_evaluators/cfg_evaluators/cfg_evaluator.h
@@ -69,7 +69,7 @@ auto DirectNeighbors(cfg::CFG &cfg, int stmt_no) -> std::vector<int> {
   }
   auto &block = opt_block.value();
   std::vector<int> neighbors;
-  int next_possible_statment = BlockIterator::NextStmt(stmt_no + 1);
+  int next_possible_statment = BlockIterator::NextStmt(stmt_no);
   if (block->IsInBlock(next_possible_statment)) {
     neighbors.push_back(next_possible_statment);
   } else {
@@ -86,11 +86,6 @@ auto DirectNeighbors(cfg::CFG &cfg, Entity *entity) -> std::vector<int> {
   return DirectNeighbors<BlockIterator>(cfg, statement->getStatementNumber());
 }
 
-//auto Next(cfg::CFG &cfg, int stmt_no) -> std::vector<int>;
-//auto Next(cfg::CFG &cfg, Entity *entity) -> std::vector<int>;
-//auto Prev(cfg::CFG &cfg, int stmt_no) -> std::vector<int>;
-//auto Prev(cfg::CFG &cfg, Entity *entity) -> std::vector<int>;
-
 template<typename T>
 auto HasNext(cfg::CFG &cfg, T stmt) -> bool {
   return !DirectNeighbors<ForwardBlockIterator>(cfg, stmt).empty();
@@ -101,255 +96,110 @@ auto HasPrev(cfg::CFG &cfg, T stmt) -> bool {
   return !DirectNeighbors<ReverseBlockIterator>(cfg, stmt).empty();
 }
 
-template<typename SrcPredicate, typename DestPredicate, typename BlockIterator, typename ProcessRow>
-class ReachableEntityFinder {
- public:
-  ReachableEntityFinder(const std::vector<Entity *> &sources,
-                        BlockIterator iterator,
-                        SrcPredicate include_src,
-                        DestPredicate include_dest,
-                        ProcessRow process_row,
-                        QueryFacade &pkb)
-      : sources_(sources), include_src_(include_src), include_dest_(include_dest),
-        process_row_(process_row), pkb_(pkb), iterator_(iterator) {}
-
-  void operator()() {
-    auto cfg = pkb_.getCFG();
-    for (const auto &source : sources_) {
-      Search(source, *cfg);
+template<typename BlockIterator, typename DestPredicate>
+auto GetEntitiesFromBlock(int src_stmt,
+                          std::shared_ptr<cfg::Block> &block,
+                          BlockIterator iterator,
+                          DestPredicate include_dest,
+                          QueryFacade &pkb) -> std::vector<Entity *> {
+  std::vector<Entity *> entities;
+  int end = iterator.NextStmt(iterator.BlockEnd(block));
+  for (int stmt = src_stmt; stmt != end; stmt = iterator.NextStmt(stmt)) {
+    auto *statement = GetStatement(stmt, pkb);
+    if (include_dest(statement)) {
+      entities.push_back(statement);
     }
-    ProcessRelationships();
   }
- private:
-  // Does a breadth first search until a statement that could be another src is reached.
-  // Search is terminated then.
-  void Search(Entity *src, cfg::CFG &cfg) {
-    int source_stmt_no = GetStmtNo(src);
-    auto opt_start = cfg.GetBlockAt(source_stmt_no);
-    if (!opt_start) {
+  return entities;
+}
+
+template<typename BlockIterator>
+auto IsWhileChild(std::shared_ptr<cfg::Block> &block,
+                  std::shared_ptr<cfg::Block> &parent,
+                  BlockIterator iterator) -> bool {
+  for (auto &nei : iterator.GetNei(block)) {
+    if (!iterator.GoingForward(iterator.BlockEnd(block), iterator.BlockStart(nei))
+        && iterator.BlockStart(nei) == iterator.BlockStart(parent)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+template<typename DestPredicate, typename BlockIterator, typename ProcessRow>
+void FindReachableEntities(const std::vector<Entity *> &sources,
+                           BlockIterator iterator,
+                           DestPredicate include_dest,
+                           ProcessRow process_row,
+                           QueryFacade &pkb) {
+  std::unordered_map<int, std::vector<Entity *>> block_cache;
+  std::unordered_map<int, std::vector<Entity *>> while_child_cache;
+  std::function<void(std::shared_ptr<cfg::Block> &block)> reachable_from;
+  std::function<void(std::shared_ptr<cfg::Block> &block)> get_while_child_entities;
+
+  reachable_from = [&](std::shared_ptr<cfg::Block> &block) {
+    if (block_cache.find(iterator.BlockStart(block)) != block_cache.end()) {
       return;
     }
-    relationships_[src] = {};
-    auto start_block = opt_start.value();
-    std::queue<std::shared_ptr<cfg::Block>> blocks;
-    blocks.push(start_block);
-    std::unordered_map<int, int> block_seen = {{iterator_.BlockStart(start_block), 1}};
-    while (!blocks.empty()) {
-      auto curr_block = blocks.front();
-      blocks.pop();
-      auto curr_block_start = iterator_.BlockStart(curr_block);
-      if (curr_block->IsInBlock(source_stmt_no) && block_seen[iterator_.BlockStart(start_block)] == 1) {
-        curr_block_start = iterator_.NextStmt(source_stmt_no);
-      }
-
-      if (!ProcessBlock(src, curr_block_start, curr_block)) {
+    std::vector<Entity *> entities_reachable = GetEntitiesFromBlock(iterator.BlockStart(block), block,
+                                                                    iterator, include_dest, pkb);
+    for (auto &nei : iterator.GetNei(block)) {
+      if (IsWhileChild(nei, block, iterator)) {
+        auto statement = GetStatement(iterator.BlockStart(block), pkb);
+        if (include_dest(statement)) {
+          entities_reachable.push_back(statement);
+        }
+        get_while_child_entities(nei);
+        auto iter = while_child_cache.find(iterator.BlockStart(nei));
+        entities_reachable.insert(entities_reachable.end(), iter->second.begin(), iter->second.end());
         continue;
       }
+      reachable_from(nei);
+      auto iter = block_cache.find(iterator.BlockStart(nei));
+      entities_reachable.insert(entities_reachable.begin(), iter->second.begin(), iter->second.end());
+    }
+    block_cache[iterator.BlockStart(block)] = std::move(entities_reachable);
+  };
 
-      for (auto &nei : iterator_.GetNei(curr_block)) {
-        auto block_start = iterator_.BlockStart(nei);
-        if (block_seen[block_start] == 2 ||
-            (iterator_.GoingForward(iterator_.BlockEnd(curr_block), block_start) && block_seen[block_start] == 1)) {
-          continue;
-        }
-        block_seen[block_start] += 1;
-        blocks.push(nei);
+  get_while_child_entities = [&](std::shared_ptr<cfg::Block> &block) {
+    if (while_child_cache.find(iterator.BlockStart(block)) != while_child_cache.end()) {
+      return;
+    }
+    std::vector<Entity *> entities_reachable = GetEntitiesFromBlock(iterator.BlockStart(block), block,
+                                                                    iterator, include_dest, pkb);
+    for (auto &nei : iterator.GetNei(block)) {
+      if (!iterator.GoingForward(iterator.BlockEnd(block), iterator.BlockStart(nei))) {
+        continue;
+      }
+      reachable_from(nei);
+      auto iter = block_cache.find(iterator.BlockStart(nei));
+      entities_reachable.insert(entities_reachable.begin(), iter->second.begin(), iter->second.end());
+    }
+    while_child_cache[iterator.BlockStart(block)] = std::move(entities_reachable);
+  };
+
+  auto &cfg = *pkb.getCFG();
+  for (const auto &source : sources) {
+    int stmt = GetStmtNo(source);
+    auto opt_start = cfg.GetBlockAt(stmt);
+    if (!opt_start) {
+      continue;
+    }
+    auto start_block = opt_start.value();
+    for (auto &nei : iterator.GetNei(start_block)) {
+      reachable_from(nei);
+      for (auto &dest : block_cache[iterator.BlockStart(nei)]) {
+        process_row(source, dest);
+      }
+    }
+    if (stmt != iterator.BlockEnd(start_block)) {
+      for (auto &dest : GetEntitiesFromBlock(iterator.NextStmt(stmt), start_block,
+                                             iterator, include_dest, pkb)) {
+        process_row(source, dest);
       }
     }
   }
-
-  auto ProcessBlock(Entity *src, int start, std::shared_ptr<cfg::Block> &block) -> bool {
-    auto curr_block_end = iterator_.BlockEnd(block);
-    for (int stmt = start; stmt != iterator_.NextStmt(curr_block_end); stmt = iterator_.NextStmt(stmt)) {
-      auto *stmt_entity = GetStatement(stmt, pkb_);
-      if (include_dest_(stmt_entity)) {
-        relationships_[src].push_back(stmt_entity);
-      }
-      if (include_src_(stmt_entity)) {
-        if (stmt_entity != src) {
-          in_degree_[src] += 1;
-          terminated_at_[stmt_entity].push_back(src);
-        }
-        return false;
-      }
-    }
-    return true;
-  }
-
-  // Does a Topo Sort
-  void ProcessRelationships() {
-    std::queue<Entity *> frontier;
-    for (auto &pair : relationships_) {
-      if (in_degree_[pair.first] == 0) {
-        frontier.push(pair.first);
-      }
-      for (auto &dest : pair.second) {
-        process_row_(pair.first, dest);
-      }
-    }
-
-    while (!frontier.empty()) {
-      Entity *entity = frontier.front();
-      frontier.pop();
-      for (auto &affected_entity : terminated_at_[entity]) {
-        for (auto &dest : relationships_[entity]) {
-          process_row_(affected_entity, dest);
-          relationships_[affected_entity].push_back(dest);
-        }
-        in_degree_[affected_entity] -= 1;
-        if (in_degree_[affected_entity] == 0) {
-          frontier.push(affected_entity);
-        }
-      }
-    }
-  }
-
-  QueryFacade &pkb_;
-  BlockIterator iterator_;
-  SrcPredicate include_src_;
-  DestPredicate include_dest_;
-  ProcessRow process_row_;
-  const std::vector<Entity *> &sources_;
-  std::unordered_map<Entity *, int> in_degree_;
-  std::unordered_map<Entity *, std::vector<Entity *>> terminated_at_;
-  std::unordered_map<Entity *, std::vector<Entity *>> relationships_;
-};
-//
-//template<typename SrcPredicate, typename DestPredicate, typename BlockIterator, typename ProcessRow>
-//auto FindReachableEntities(const std::vector<Entity *> &sources,
-//                           BlockIterator iterator,
-//                           SrcPredicate include_src,
-//                           DestPredicate include_dest,
-//                           ProcessRow process_row,
-//                           QueryFacade &pkb) {
-//  auto *cfg = pkb.getCFG();
-//  std::unordered_map<Entity *, std::vector<Entity *>> terminated_at;
-//  std::unordered_map<Entity *, std::vector<Entity *>> relationships;
-//
-//  for (const auto &source : sources) {
-//    int source_stmt_no = GetStmtNo(source);
-//    auto opt_start = cfg->GetBlockAt(source_stmt_no);
-//    if (!opt_start) {
-//      continue;
-//    }
-//    auto start_block = opt_start.value();
-//    std::queue<std::shared_ptr<cfg::Block>> blocks;
-//    blocks.push(start_block);
-//    std::unordered_map<int, int> block_seen = {{iterator.BlockStart(start_block), 1}};
-//    while (!blocks.empty()) {
-//      auto curr_block = blocks.front();
-//      blocks.pop();
-//      auto curr_block_start = iterator.BlockStart(curr_block);
-//      auto curr_block_end = iterator.BlockEnd(curr_block);
-//      if (curr_block->IsInBlock(source_stmt_no) && block_seen[iterator.BlockStart(start_block)] == 1) {
-//        curr_block_start = iterator.NextStmt(source_stmt_no);
-//      }
-//
-//      bool discontinue = false;
-//      for (int stmt = curr_block_start; stmt != iterator.NextStmt(curr_block_end);
-//           stmt = iterator.NextStmt(stmt)) {
-//        auto *stmt_entity = GetStatement(stmt, pkb);
-//        if (include_dest(stmt_entity)) {
-//          relationships[source].push_back(stmt_entity);
-//        }
-//        if (include_src(stmt_entity)) {
-//          if (stmt_entity != source) {
-//            terminated_at[source].push_back(stmt_entity);
-//          }
-//          discontinue = true;
-//          break;
-//        }
-//      }
-//      if (discontinue) {
-//        break;
-//      }
-//
-//      for (auto &nei : iterator.GetNei(curr_block)) {
-//        auto block_start = iterator.BlockStart(nei);
-//        if (block_seen[block_start] == 2 ||
-//            (iterator.GoingForward(curr_block_end, block_start) && block_seen[block_start] == 1)) {
-//          continue;
-//        }
-//        block_seen[block_start] += 1;
-//        blocks.push(nei);
-//      }
-//    }
-//  }
-//
-//  for (auto &relationship : relationships) {
-//    for (auto &dest : relationship.second) {
-//      process_row(relationship.first, dest);
-//    }
-//  }
-//
-//  for (auto &srcs : terminated_at) {
-//    for (auto &reachable_src : srcs.second) {
-//      for (auto &dest : relationships[reachable_src]) {
-//        process_row(srcs.first, dest);
-//      }
-//    }
-//  }
-//}
-
-
-
-//template<typename SrcPredicate, typename DestPredicate, typename BlockIterator, typename ProcessRow>
-//void (const std::vector<Entity *> &sources,
-//                           BlockIterator iterator,
-//                           SrcPredicate include_src,
-//                           DestPredicate include_dest,
-//                           ProcessRow process,
-//                           QueryFacade &pkb) {
-//  auto *cfg = pkb.getCFG();
-//  std::unordered_set<int> seen;
-//  for (const auto &source : sources) {
-//    int source_stmt_no = GetStmtNo(source);
-//    if (seen.find(source_stmt_no) != seen.end()) {
-//      continue;
-//    }
-//    auto opt_start = cfg->GetBlockAt(source_stmt_no);
-//    if (!opt_start) {
-//      continue;
-//    }
-//    std::unordered_set<Entity *> active_src{source};
-//    auto start_block = opt_start.value();
-//    std::unordered_map<int, int> block_seen = {{iterator.BlockStart(start_block), 1}};
-//    std::queue<std::shared_ptr<cfg::Block>> blocks;
-//    blocks.push(start_block);
-//    while (!blocks.empty()) {
-//      auto curr_block = blocks.front();
-//      blocks.pop();
-//      auto curr_block_start = iterator.BlockStart(curr_block);
-//      auto curr_block_end = iterator.BlockEnd(curr_block);
-//      if (curr_block->IsInBlock(source_stmt_no) && block_seen[iterator.BlockStart(start_block)] == 1) {
-//        curr_block_start = iterator.NextStmt(source_stmt_no);
-//      }
-//      for (int stmt = curr_block_start; stmt != iterator.NextStmt(curr_block_end);
-//           stmt = iterator.NextStmt(stmt)) {
-//        auto *stmt_entity = GetStatement(stmt, pkb);
-//        if (include_dest(stmt_entity)) {
-//          for (auto *src : active_src) {
-//            std::cout << GetStmtNo(src) << " " << stmt << "\n";
-//            process(src, stmt_entity);
-//          }
-//        }
-//        if (include_src(stmt_entity)) {
-//          active_src.insert(stmt_entity);
-//          seen.insert(stmt);
-//        }
-//      }
-//      for (auto &nei : iterator.GetNei(curr_block)) {
-//        auto block_start = iterator.BlockStart(nei);
-//        if (block_seen[block_start] == 2 ||
-//            (iterator.GoingForward(curr_block_end, block_start) && block_seen[block_start] == 1)) {
-//          continue;
-//        }
-//        block_seen[block_start] += 1;
-//        blocks.push(nei);
-//      }
-//    }
-//  }
-//}
+}
 
 template<typename BlockIterator>
 auto FindReachableEntities(int src, EntityType dest, QueryFacade &pkb) -> std::vector<Entity *> {
@@ -358,12 +208,11 @@ auto FindReachableEntities(int src, EntityType dest, QueryFacade &pkb) -> std::v
     return {};
   }
   std::vector<Entity *> dest_entities;
-  ReachableEntityFinder({entity},
+  FindReachableEntities({entity},
                         BlockIterator{},
-                        [](Entity *entity) { return false; },
                         [&](Entity *entity) { return MatchesEntityType(entity, dest); },
                         [&](Entity *src, Entity *dst) { dest_entities.push_back(dst); },
-                        pkb)();
+                        pkb);
   return dest_entities;
 }
 
@@ -373,12 +222,11 @@ auto FindReachableEntities(const std::vector<Entity *> src,
                            EntityType dest_type,
                            QueryFacade &pkb) -> std::vector<std::vector<Entity *>> {
   std::vector<std::vector<Entity *>> relationships;
-  ReachableEntityFinder(src,
+  FindReachableEntities(src,
                         BlockIterator{},
-                        [&](Entity *entity) { return MatchesEntityType(entity, src_type); },
                         [&](Entity *entity) { return MatchesEntityType(entity, dest_type); },
                         [&](Entity *src, Entity *dst) { relationships.push_back({src, dst}); },
-                        pkb)();
+                        pkb);
   return relationships;
 }
 
@@ -389,12 +237,11 @@ auto Reachable(int src, int dest, QueryFacade &pkb) -> bool {
   if (entity == nullptr) {
     return found;
   }
-  ReachableEntityFinder({entity},
+  FindReachableEntities({entity},
                         BlockIterator{},
-                        [src](Entity *entity) { return false; },
                         [dest](Entity *entity) { return GetStmtNo(entity) == dest; },
                         [&](Entity *src, Entity *dst) { found = true; },
-                        pkb)();
+                        pkb);
   return found;
 }
 

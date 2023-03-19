0a52a989177d3f8e5389851242025bcbfd6d4243
zhongfu <me@zhongfu.li>
2023-03-19 10:40:31 +0800

    refactor(util): clang-tidy fixes, efficiency, std::all_of instead of for loop

--- Team21/Code21/src/spa/src/util/is_identifier.cpp
+++ Team21/Code21/src/spa/src/util/is_identifier.cpp
@@ -1,18 +1,15 @@
+#include <algorithm>
 #include <string>
 
 namespace util {
-    bool is_identifier(const std::string& str) {
+    auto is_identifier(std::string_view str) -> bool {
         if (str.length() == 0) {
             return false;
         }
         if (std::isdigit(str[0]) != 0) {
             return false;
         }
-        for (const char& c : str) {
-            if (std::isalnum(c) == 0) {
-                return false;
-            }
-        }
-        return true;
+        return std::all_of(str.begin(), str.end(),
+                           [](char chr) { return std::isalnum(chr); });
     }
 }
--- Team21/Code21/src/spa/src/util/unique_ptr_cast.h
+++ Team21/Code21/src/spa/src/util/unique_ptr_cast.h
@@ -7,7 +7,7 @@ namespace std {
    * Note: this will cause the original pointer to be zeroed out! Be careful.
   */
   template <typename Derived, typename Base>
-  std::unique_ptr<Derived> static_pointer_cast(std::unique_ptr<Base>&& p) {
+  auto static_pointer_cast(std::unique_ptr<Base>&& p) -> std::unique_ptr<Derived> {
     return std::unique_ptr<Derived>(static_cast<Derived*>(p.release()));
   }
 }
